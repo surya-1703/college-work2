72d5f8bd7278ef77b6cb454a9a3ada491d3a283e
Surya s <surya20070317@gmail.com>
2026-07-06 10:54:12 +0530

    First Commit

---
+++ Grade.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Grade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks (0-100): ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Grade: A");
+        } else if (marks >= 80) {
+            System.out.println("Grade: B");
+        } else if (marks >= 70) {
+            System.out.println("Grade: C");
+        } else if (marks >= 60) {
+            System.out.println("Grade: D");
+        } else if (marks >= 50) {
+            System.out.println("Grade: E");
+        } else {
+            System.out.println("Grade: F (Fail)");
+        }
+
+        sc.close();
+    }
+}
